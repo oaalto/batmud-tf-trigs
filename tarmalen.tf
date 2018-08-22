7f91d907a9dca28359a01344e7c5d25050cd7702
Olli Aalto <oaalto@gmail.com>
2018-08-22 07:42:07 +0300

    ..

--- tarmalen.tf
+++ tarmalen.tf
@@ -1,4 +1,8 @@
 ;; Cure Spells
 /def -p10 -F -P1BCcyan -mregexp -t"You heal (little) of your own wounds."
-/def -p10 -F -P1BCgreen -mregexp -t"You heal (much) of your own wounds."
+/def -p10 -F -P1BCyellow -mregexp -t"You heal (much) of your own wounds."
 /def -p10 -F -aBCgreen -mregexp -t"^[^ ]+ is not wounded."
+
+/def -p10 -F -P2BCyellow -mregexp -t"(.+)'s spell makes you feel (some) better!"
+/def -p10 -F -P2Cgreen -mregexp -t"(.+)'s spell makes you feel (very much) better!"
+/def -p10 -F -P2BCgreen -mregexp -t"(.+)'s spell makes you feel (all) better!"

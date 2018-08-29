57c43dca6a895b05e27c0bb01102bb294d0a621f
Olli Aalto <oaalto@gmail.com>
2018-08-29 12:06:51 +0300

    ..

--- riftwalker.tf
+++ riftwalker.tf
@@ -1,19 +1,45 @@
+;; Entity Skills
+/set fire_skill=blazing sunder
+/set air_skill=suffocating embrace
+
+/def set_entity_skill =\
+	/if ({1} == "fire") \
+		/set current_skill=%{fire_skill} %;\
+	/endif %;\
+	/if ({1} == "air") \
+		/set current_skill=%{air_skill} %;\
+	/endif
+
+/alias ufire /set_entity_skill fire
+/alias uair /set_entity_skill air
+
+/def use_current_skill =\
+	/if (strlen({1}) > 0)\
+		/send @gem cmd use %{current_skill} at %{*} %;\
+	/else \
+		/send @gem cmd use %{current_skill} %;\
+	/endif
+	
+/alias ccs /use_current_skill %{*}
+
+/def summon_entity =\
+	/send @cast summon rift entity at %{1} %;\
+	/set_entity_skill %{1}
+
+/alias csum /summon_entity %{*}
+
+/def transform_entity =\
+	/send @cast transform rift entity at %{1} %;\
+	/set_entity_skill %{1}
+
+/alias cte /transform_entity %{*}
+
 ;; Start Battle
 /def start_battle =\
 	/cast_spark_birth %{*} %;\
-	/use_blazing_sunder %{*} %;\
+	/use_current_skill %{*} %;\
 
 /alias css /start_battle %{*}
-
-;; Blazing Sunder
-/def use_blazing_sunder =\
-	/if (strlen({1}) > 0)\
-		/send @gem cmd use blazing sunder at %{*} %;\
-	/else \
-		/send @gem cmd use blazing sunder %;\
-	/endif
-
-/alias cbs /use_blazing_sunder %{*}
 
 ;; Spark Birth
 /def cast_spark_birth =\
@@ -36,7 +62,7 @@
 /alias crp /cast_rift_pulse %{*}
 
 /def -p10 -F -aBCred -msimple -t"Your fire entity does some strange combat maneuver but doesn\'t hit anything."
-/def -p10 -F -P1BCwhite -mregexp -t"(Entity sense:) (.+)"
+/def -p10 -F -P1BCblue -mregexp -t"(Entity sense:) (.+)"
 
 
 

d37498813cc45b6673c9239df25bec8cc531124b
Olli Aalto <oaalto@gmail.com>
2018-08-30 19:26:25 +0300

    ..

--- riftwalker.tf
+++ riftwalker.tf
@@ -93,6 +93,10 @@
 /def -p10 -F -aBCred -msimple -t"Your air entity falters and its wispy tendrils fall to its sides." =\
 	/echo -aBCred SUFFOCATING EMBRACE IS DOWN!
  
+/def -p10 -F -aBCred -msimple -t"Your entity loses its concentration and cannot do the skill." =\
+	/if ({current_skill =~ {air_skill})\
+			/echo -aBCred SUFFOCATING EMBRACE IS DOWN! %;\
+	/endif
 
 
 

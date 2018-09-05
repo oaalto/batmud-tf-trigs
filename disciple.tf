1c17e1a25ca675a3dbfcf455e1afdb9c8f757bcc
Olli Aalto <oaalto@gmail.com>
2018-09-05 08:31:56 +0300

    ..

--- disciple.tf
+++ disciple.tf
@@ -1,6 +1,6 @@
 
 /def -p10 -F -aBCred -msimple -t"You feel like the pulse of chaos inside you is slowing down!" =\
-	/echo -aBCred --------------- SPAWN GOING DOWN!! --------------- %;\
+	/echo -aBCred *************** SPAWN GOING DOWN!! *************** %;\
 
 
 

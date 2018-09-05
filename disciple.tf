7289b0e080bf7185541c9b12d03d73b79739571a
Olli Aalto <oaalto@gmail.com>
2018-09-05 08:29:10 +0300

    ..

---
+++ disciple.tf
@@ -0,0 +1,10 @@
+
+/def -p10 -F -aBCred -msimple -t"You feel like the pulse of chaos inside you is slowing down!" =\
+	/echo -aBCred --------------- SPAWN GOING DOWN!! --------------- %;\
+
+
+
+
+
+
+
--- tzarakk.tf
+++ tzarakk.tf
@@ -1,7 +1,7 @@
 
  
 /def -p10 -F -aBCred -msimple -t"You are knocked off your mount!" =\
-	/echo -aBCred DISMOUNTED!! %;\
+	/echo -aBCred --------------- DISMOUNTED!! --------------- %;\
 
 
 

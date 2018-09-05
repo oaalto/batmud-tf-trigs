2342fedd320d9c29c83451c41febc04505417744
Olli Aalto <oaalto@gmail.com>
2018-09-05 13:25:37 +0300

    ..

--- tzarakk.tf
+++ tzarakk.tf
@@ -1,7 +1,9 @@
-
- 
 /def -p10 -F -aBCred -msimple -t"You are knocked off your mount!" =\
 	/echo -aBCred *************** DISMOUNTED!! *************** %;\
+
+/def -p10000 -F -mregexp -t'^[\*]+ Round .* [\*]+$' battle_round_tzarakk =\
+	/send @x vedir
+
 
 
 

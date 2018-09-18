e2456dc9e4ab3d981dff41358d4e110a912f7cba
Olli Aalto <oaalto@gmail.com>
2018-09-18 11:29:59 +0300

    ..

--- tzarakk.tf
+++ tzarakk.tf
@@ -4,9 +4,18 @@
 /def -p10000 -F -mregexp -t'^[\*]+ Round .* [\*]+$' battle_round_tzarakk =\
 	/send @x vedir
 
+/def -p10000 -F -mregexp -t'^A faint fog-like substance flows from corpse of (.*) to Vedir's lifeless eyes replenishing it (.*)' harvest_soul =\
+	/send @feed %;\
+	/send @x vedir
 
+/def set_feed_mode =\
+	/send @rip_action set get all from corpse;tzarakk chaosfeed corpse;tzarakk chaosfeed corpse;drop zinc;drop mowgles
 
+/alias feed_mode /set_feed_mode
+/set_feed_mode
 
+/def set_heal_mode =\
+	/send @rip_action set get all from corpse;use harvest soul at corpse;drop zinc;drop mowgles
 
-
+/alias heal_mode /set_heal_mode
 

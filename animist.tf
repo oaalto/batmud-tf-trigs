ceab05f771c4f8bc0f3efcd197b56bad66f368b6
Olli Aalto <oaalto@gmail.com>
2019-02-05 09:29:16 +0200

    .

--- animist.tf
+++ animist.tf
@@ -1,1 +1,27 @@
+/set cere_done=0
+/set separating_soul=0
+
 /def -p10000 -F -mregexp -P3BCwhite -t'(Your soul companion): (.+) \((.+)\) (.+)'
+
+/def -p10000 -F -msimple -t'You start chanting.' start_spell =\
+	/set cere_done=0
+
+/def -p10000 -F -msimple -t'You are done with the chant.' start_separating =\
+	/set separating_soul=0
+
+/def -p10000 -F -msimple -t'You perform the ceremony.' cere_success =\
+	/set cere_done=1 %;\
+	/if ({separating_soul})\
+		/cast_separate_soul %;\
+	/endif
+
+;; Separate Soul
+/def cast_separate_soul =\
+	/if ({cere_done})\
+		/send @cast separate soul %;\
+	/else \
+		/set separating_soul=1 %;\
+		/use_ceremony %;\
+	/endif
+
+/alias csoul /cast_separate_soul
--- tarmalen.tf
+++ tarmalen.tf
@@ -1,3 +1,4 @@
+
 ;; Cure Spells
 /def -p10 -F -P1BCcyan -mregexp -t"You heal (little) of your own wounds."
 /def -p10 -F -P1BCyellow -mregexp -t"You heal (much) of your own wounds."
@@ -6,3 +7,10 @@
 /def -p10 -F -P2BCyellow -mregexp -t"(.+)'s spell makes you feel (some) better!"
 /def -p10 -F -P2Cgreen -mregexp -t"(.+)'s spell makes you feel (very much) better!"
 /def -p10 -F -P2BCgreen -mregexp -t"(.+)'s spell makes you feel (all) better!"
+
+;; Ceremony
+/def use_ceremony =\
+	/send @use ceremony %;\
+
+/alias cere /use_ceremony %{*}
+

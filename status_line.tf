11ffc71fdcdd6c06ae102f85b0ec9a69f73582c7
Olli Aalto <oaalto@gmail.com>
2019-04-18 18:01:37 +0300

    .

--- status_line.tf
+++ status_line.tf
@@ -8,18 +8,21 @@
 /set status_pad= 
 /set visual_mode=0
 
-/def -p1000 -F -ag -mregexp -t'^H:(-?[0-9]+)/(-?[0-9]+) \[[+-]?[0-9]*\] S:(-?[0-9]+)/(-?[0-9]+) \[[+-]?[0-9]*\] E:(-?[0-9]+)/(-?[0-9]+) \[[+-]?[0-9]*\] \$:(-?[0-9]+) \[[+-]?[0-9]*\] exp:(-?[0-9]+) \[[+-]?[0-9]*\]$' = \
-	/set hp_var=%{P1}/%{P2} %;\
-	/set sp_var=%{P3}/%{P4} %;\
-	/set ep_var=%{P5}/%{P6} %;\
-	/set money_var=%{P7} %;\
-	/set exp_var=%{P8}
+/def set_status_line = \
+	/set hp_var=$[decode_attr({1}, colorer({1}, {2}))]/%{2} %;\
+        /set sp_var=$[decode_attr({3}, colorer({3}, {4}))]/%{4} %;\
+        /set ep_var=$[decode_attr({5}, colorer({5}, {6}))]/%{6} %;\
+        /set exp_var=%{7} %;\
+	/if (strlen({8}) > 0)\
+		/set money_var=%{8} %;\
+	/endif %;\
+	/prompt >
 
-/set hp_var=?/?
-/set sp_var=?/?
-/set ep_var=?/?
-/set money_var=?
-/set exp_var=?
+/def -p1000 -F -ag -mregexp -t'^H:(-?[0-9]+)/(-?[0-9]+) \[[+-]?[0-9]*\] S:(-?[0-9]+)/(-?[0-9]+) \[[+-]?[0-9]*\] E:(-?[0-9]+)/(-?[0-9]+) \[[+-]?[0-9]*\] \$:(-?[0-9]+) \[[+-]?[0-9]*\] exp:(-?[0-9]+) \[[+-]?[0-9]*\]$' = \
+	/set_status_line %{P1} %{P2} %{P3} %{P4} %{P5} %{P6} %{P7} %{P8}
+
+/def -i -p9999 -mregexp -h"PROMPT Hp:(.+)/(.+) Sp:(.+)/(.+) Ep:(.+)/(.+) Exp:(.+) >$" = \
+	/set_status_line %{P1} %{P2} %{P3} %{P4} %{P5} %{P6} %{P7}
 
 /status_add -c 'Hp: ' hp_var:9:Cwhite ' Sp: ' sp_var:9:Cwhite ' Ep: ' ep_var:9:Cwhite ' $: ' money_var:10:Cyellow ' Exp: ' exp_var::Cwhite
 

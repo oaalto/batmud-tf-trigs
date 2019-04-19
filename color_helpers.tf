298921a63c11c2dbb324c4606acd1fbb1f367c92
Olli Aalto <oaalto@gmail.com>
2019-04-19 19:49:38 +0300

    .

--- color_helpers.tf
+++ color_helpers.tf
@@ -1,19 +1,19 @@
 /def colorhelper = \
-        /if (regmatch("[0-9]+", {1}) == 0) /return "xC8"%;\
-        /elseif ({1} > 80) /return "xhCgreen"%;\
-        /elseif ({1} > 60) /return "xCgreen"%;\
-        /elseif ({1} > 40) /return "xCyellow"%;\
-        /elseif ({1} > 20) /return "xhCred"%;\
-        /elseif ({1} > 0) /return "xCred"%;\
-        /else /return "xrCred"%;\
+        /if ({1} > 90) /return "xBCgreen" %;\
+	/elseif ({1} > 80) /return "xCgreen" %;\
+        /elseif ({1} > 66) /return "xBCcyan" %;\
+        /elseif ({1} > 50) /return "xCcyan" %;\
+	/elseif ({1} > 35) /return "xBCyellow" %;\
+        /elseif ({1} > 20) /return "xCyellow" %;\
+	/elseif ({1} > 10) /return "xBCred" %;\
+        /else /return "xCred" %;\
         /endif
 
 /def colorer = \
         /let ratio=$[{1} * 100 / {2}] %;\
         /return colorhelper(ratio)
 
-/def set_white = /return decode_attr({1}, 'BCwhite')
+/def set_white = /return decode_attr({1}, 'xBCwhite')
 
 /def color_str = /return decode_attr({1}, colorer({1}, {2}))
-
 

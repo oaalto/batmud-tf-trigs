fa7c2dbbcd7333e2b6c23086624f7de68a52c164
Olli Aalto <oaalto@gmail.com>
2019-05-03 12:05:29 +0300

    .

--- status_line.tf
+++ status_line.tf
@@ -9,30 +9,24 @@
 /set visual_mode=0
 
 /def set_status_line = \
-	/let hp=$[color_str({1}, {2})]/$[set_white({2})] %;\
-        /let sp=$[color_str({3}, {4})]/$[set_white({4})] %;\
-        /let ep=$[color_str({5}, {6})]/$[set_white({6})] %;\
+	/let hp=$[color_str({1}, {2})]/$[set_white({2})] %{hpDiff} %;\
+        /let sp=$[color_str({3}, {4})]/$[set_white({4})] %{spDiff} %;\
+        /let ep=$[color_str({5}, {6})]/$[set_white({6})] %{epDiff} %;\
         /let exp=$[set_white({7})] %;\
 	/set status_line_1=Hp: %{hp} Sp: %{sp} Ep: %{ep} \$: %{money} %{moneyDiff} Exp: %{exp} %{expDiff} %;\
 	/prompt >
 
-/def -p1000 -F -ag -mregexp -t'^H:(.+)/(.+) \[[+-]?[0-9]*\] S:(.+)/(.+) \[[+-]?[0-9]*\] E:(.+)/(.+) \[[+-]?[0-9]*\] \$:(.+) \[(.*)\] exp:(.+) \[(.*)\]$' = \
-	/if (strlen({P7}) > 0)\
-;; Set because otherwise it will display an empty value
-                /set money=$[set_white({P7})] %;\
-        /endif %;\
-	/if (strlen({P8}) > 0) \
-                /set moneyDiff=(%{P8}) %;\
-                /repeat -5 1 /eval /set moneyDiff=%;\
-        /endif %;\
-        /if (strlen({P10}) > 0) \
-                /set expDiff=(%{P10}) %;\
-                /repeat -5 1 /eval /set expDiff=%;\
-        /endif %;\
-	/set_status_line %{P1} %{P2} %{P3} %{P4} %{P5} %{P6} %{P9}
+/def -p1000 -F  -mregexp -t'^H:(.+)/(.+) (\[.*\]) S:(.+)/(.+) (\[.*\]) E:(.+)/(.+) (\[.*\]) \$:(.+) (\[.*\]) exp:(.+) (\[.*\])$' = \
+	/set hpDiff=%{P3} %;\
+	/set spDiff=%{P6} %;\
+	/set epDiff=%{P9} %;\
+	/set money=$[set_white({P10})] %;\
+        /set moneyDiff=%{P11} %;\
+        /set expDiff=%{P13} %;\
+	/set_status_line %{P1} %{P2} %{P4} %{P5} %{P7} %{P8} %{P12}
 
 /def -i -p9999 -mregexp -h"PROMPT Hp:(.+)/(.+) Sp:(.+)/(.+) Ep:(.+)/(.+) Exp:(.+) >$" = \
-	/set_status_line %{P1} %{P2} %{P3} %{P4} %{P5} %{P6} %{P7}
+	/test $[set_status_line({P1}, {P2}, {P3}, {P4}, {P5}, {P6}, {P7})]
 
 /status_add -c -r0 status_line_1
 /status_add -c -r1 status_line_2

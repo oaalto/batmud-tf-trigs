ea0c3550a7b32308c0e38aea30ffee45f8e3b707
Olli Aalto <oaalto@gmail.com>
2019-05-17 14:28:17 +0300

    .

--- liberator.tf
+++ liberator.tf
@@ -5,45 +5,51 @@
 ;; SPELLS
 
 ;; Ghost light
-/def cast_ghost_light =\
+/def cast_ghost_light = \
 	/send @target %{*};liberator select weakest non armoursmith,guardian,weaponsmith,soldier;cast 'ghost light' %{*}
 
 /alias cgl /cast_ghost_light %{*}
 
 ;; Ghost chill
-/def cast_ghost_chill =\
+/def cast_ghost_chill = \
 	/send @target %{*};liberator select weakest non armoursmith,guardian,weaponsmith,soldier;cast 'ghost chill' %{*}
 
 /alias cgc /cast_ghost_chill %{*}
 
 ;; Ghost armour
-/def cast_ghost_armour =\
+/def cast_ghost_armour = \
 	/send @liberator select weakest armoursmith;cast ghost armour %;\
 
 /alias cga /cast_ghost_armour
 
 ;; Ghost link
-/def cast_ghost_link =\
+/def cast_ghost_link = \
 	/send @liberator select weakest non armoursmith,guardian,weaponsmith,soldier;cast ghost link at me %;\
 
 /alias clink /cast_ghost_link
 
 ;; Restful sleep
-/def cast_restful_sleep =\
+/def cast_restful_sleep = \
 	/send @liberator select weakest non armoursmith,guardian,weaponsmith,soldier;cast restful sleep %;\
 
 /alias crs /cast_restful_sleep
 
+;; Holy glow
+/def cast_holy_glow = \
+	/send @cast holy glow
+
+/alias chg /cast_holy_glow
+
 ;; SKILLS
 
 ;; Ghost slash
-/def use_ghost_slash =\
+/def use_ghost_slash = \
 	/send @target %{*};liberator select weakest soldier;use 'ghost slash' %{*}
 
 /alias ugs /use_ghost_slash %{*}
 
 ;; Radiant slash
-/def use_radiant_slash =\
+/def use_radiant_slash = \
 	/send @target %{*};liberator select weakest non armoursmith,guardian,weaponsmith,soldier;use 'radiant slash' %{*}
 
 /alias urs /use_radiant_slash %{*}

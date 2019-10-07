9dfd9471c5ce2a08208f64479335f03388abd622
Olli Aalto <oaalto@gmail.com>
2019-10-07 09:36:03 +0300

    .

--- tiger.tf
+++ tiger.tf
@@ -1,11 +1,11 @@
 ;; Iron Palm
-/def use_iron_palm =\
+/def use_iron_palm = \
 	/send @target %{*};use 'iron palm' %{*}
 
 /alias ip /use_iron_palm %{*}
 
-/def use_dim_mak =\
-	/if (strlen({1}) > 0)\
+/def use_dim_mak = \
+	/if (strlen({1}) > 0) \
 		/send @target %{*};use dim mak at %{*} %;\
 	/else \
 		/echo No target! %;\
@@ -14,20 +14,27 @@
 /alias dm /use_dim_mak %{*}
 
 ;; Meditation
-/def use_meditation =\
+/def use_meditation = \
 	/send @use meditation %;\
 
 /alias med /use_meditation
 
 ;; Tiger Claw
-/def cast_tiger_claw =\
+/def cast_tiger_claw = \
 	/send @target %{*};cast 'tiger claw' %{*}
 
 /alias tc /cast_tiger_claw %{*}
 
 ;; Flame Fists
-/def cast_flame_fists =\
+/def cast_flame_fists = \
         /send @cast flame fists %;\
  
 /alias cff /cast_flame_fists
 
+;; Invisibility
+/def cast_invisibility = \
+	/if (strlen({1}) > 0) \
+		/send cast invisibility at %{*} %;\
+	/else \
+		/send cast invisiblity at me %;\
+	/endif

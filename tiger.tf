36a326974b3077513825e278d1775f27e38b3f58
Olli Aalto <oaalto@gmail.com>
2019-10-07 12:32:27 +0300

    .

--- tiger.tf
+++ tiger.tf
@@ -41,3 +41,13 @@
 
 /alias cinv /cast_invisibility %{*}
 
+;; Water Walking
+/def cast_water_walking = \
+        /if (strlen({1}) > 0) \
+                /send cast water walking at %{*} %;\
+        /else \
+                /send cast water walking at me %;\
+        /endif
+
+/alias cww /cast_water_walking %{*}
+

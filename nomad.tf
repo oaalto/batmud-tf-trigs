4073a9e87c9d2078715240296f8c4292fc4b9e38
Olli Aalto <oaalto@gmail.com>
2019-10-30 15:39:49 +0200

    .

---
+++ nomad.tf
@@ -0,0 +1,12 @@
+;; Fire building
+/def use_fire_building = \
+        /send @use fire building
+
+/alias ufb /use_fire_building 
+
+;; Camping
+/def use_camping = \
+        /send @use camping
+
+/alias camp /use_camping
+

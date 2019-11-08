33b82fff25a9e68ea3cdce6784b83141c0b21401
Olli Aalto <oaalto@gmail.com>
2019-11-08 10:35:45 +0200

    .

---
+++ misc.tf
@@ -0,0 +1,10 @@
+;; Boons
+
+;; Leecher
+/def -p10 -F -aBCgreen -msimple -t"You leech some of your foes energy."
+
+;; Races
+
+;; Minotaur
+/def -p10 -F -aBCyellow -msimple -t"You realize a more effective way to use your horns!"
+

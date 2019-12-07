4792a2beed9695e0253317d6f719298167b9d287
Olli Aalto <oaalto@gmail.com>
2019-12-07 09:24:27 +0200

    .

---
+++ folklorist.tf
@@ -0,0 +1,18 @@
+;; Study creature
+/def use_study_creature = \
+	/send @use study creature at %{*}
+
+/alias usc /use_study_creature %{*}
+
+;; Eye of loraen
+/def use_eye_of_loraen = \
+	/send @use eye of loraen at %{*}
+
+/alias uel /use_eye_of_loraen %{*}
+
+;; Plant lore
+/def use_plant_lore = \
+	/send @use plant lore at %{*}
+
+/alias upl /use_plant_lore %{*}
+

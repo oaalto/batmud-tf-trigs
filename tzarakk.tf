945727eef8ed03ffff658b75b681362e92094c94
Olli Aalto <oaalto@gmail.com>
2020-01-10 10:30:19 +0200

    .

--- tzarakk.tf
+++ tzarakk.tf
@@ -32,7 +32,7 @@
 /def -p10000 -F -msimple -t'You get up on Vedir and begin to ride.' = /set dismounted=0
 
 /def set_feed_mode = \
-	/send @rip_action set get all from corpse;tzarakk chaosfeed corpse;drop zinc;drop mowgles
+	/send @rip_action set get all from corpse;tzarakk chaosfeed corpse;tzarakk chaosfeed corpse;drop zinc;drop mowgles
 
 /alias feed_mode /set_feed_mode
 
@@ -40,6 +40,11 @@
 	/send @rip_action set get all from corpse;use harvest soul at corpse;drop zinc;drop mowgles
 
 /alias heal_mode /set_heal_mode
+
+/def set_hunt_mode = \
+        /send @rip_action set get all from corpse;tzarakk chaosfeed corpse;drop zinc;drop mowgles
+
+/alias hunt_mode /set_hunt_mode
 
 /def do_sleep = \
     /send @dismount;sleep

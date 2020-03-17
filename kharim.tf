e00f870bfb69d645c3e6d77ce2d9d24f8a3dde0b
Olli Aalto <oaalto@gmail.com>
2020-03-17 13:26:58 +0200

    .

--- kharim.tf
+++ kharim.tf
@@ -3,39 +3,54 @@
 
 ;; Foul Play
 /def use_foul_play = \
-	/send @kharim observe;target %{*};use 'foul play' %{*}
+	/if (strlen({1}) > 0) \
+		/send @kharim observe;target %{*} %;\
+	/endif %;\
+	/send @use 'foul play' %{*}
 
 /alias ufp /use_foul_play %{*}
 
 ;; Deceitful act of mercy
 /def use_act_of_mercy = \
-	/send @target %{*};use 'deceitful act of mercy' %{*}
+	/if (strlen({1}) > 0) \
+		/send @target %{*} %;\
+	/endif %;\
+	/send @use 'deceitful act of mercy' %{*}
 
 /alias uam /use_act_of_mercy %{*}
 
 ;; Feigned remorse
 /def use_feigned_remorse = \
-	/send @target %{*};use 'feigned remorse' %{*}
+	/if (strlen({1}) > 0) \
+		/send @target %{*} %;\
+	/endif %;\
+	/send @use 'feigned remorse' %{*}
 
 /alias ufr /use_feigned_remorse
 
 ;; Scourge of dark steel
 /def use_scourge = \
-	/send @kharim observe;target %{*};use 'scourge of dark steel' %{*}
+	/if (strlen({1}) > 0) \
+		/send @kharim observe;target %{*} %;\
+	/endif %;\
+	/send @use 'scourge of dark steel' %{*}
 
 /alias usd /use_scourge %{*}
 
 ;; Vampiric blow
 /def use_vampiric_blow = \
-	/send @target %{*};use 'vampiric blow' %{*}
+	/if (strlen({1}) > 0) \
+		/send @target %{*} %;\
+	/endif %;\
+	/send @use 'vampiric blow' %{*}
 
 /alias uvb /use_vampiric_blow %{*}
 
 ;; Chaotic circulation
 /def use_chaotic_circulation = \
-	/send @use Chaotic circulation %;\
+	/send @use Chaotic circulation at me %;\
 
-/alias ucc /use_chaotic_circulation %{*}
+/alias ucc /use_chaotic_circulation
 
 ;; Rip action
 /def kharim_rip_action = /send @rip_action set get all from corpse;kharim drain corpse;drop zinc;drop mowgles

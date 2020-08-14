e5c728665d63f4478165f632d476692d3acd2279
Olli Aalto <oaalto@gmail.com>
2020-08-14 10:35:07 +0300

    .

---
+++ spider.tf
@@ -0,0 +1,51 @@
+/def cast_hunger = \
+	/send @target %{*};cast hunger of the spider at %{*}
+
+/alias ch /cast_hunger %{*}
+
+/def cast_spider_conjuration = \
+	/send @cast spider demon conjuration at %{*}
+
+/alias csum /cast_spider_conjuration %{*}
+
+/def cast_spider_control = \
+	/send @cast spider demon control
+
+/alias ctrl /cast_spider_control
+
+/def cast_spider_sacrifice = \
+	/send @cast spider demon sacrifice at %{*}
+
+/alias csac /cast_spider_sacrifice %{*}
+
+/def cast_spider_banishment = \
+	/send @cast spider demon banishment
+
+/alias cdis /cast_spider_banisment
+
+/def cast_spider_inquiry = \
+	/send @cast spider demon inquiry
+
+/alias cinq /cast_spider_inquiry
+
+/def cast_spider_channeling = \
+	/send @cast spider demon channeling
+
+/alias cchan /cast_spider_channeling
+
+/def cast_toxic_dilution = \
+	/send @cast 'toxic dilution' %{*}
+
+/alias ctd /cast_toxic_dilution %{*}
+
+/def cast_venom_blade = \
+	/send @cast 'venom blade' %{*}
+
+/alias cvb /cast_venom_blade %{*}
+
+/def use_stab = \
+	/send @use 'stab' %{*}
+
+/alias us /use_stab %{*}
+
+

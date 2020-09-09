a14630db367f33268eb12cca244683135d39dc96
Olli Aalto <oaalto@gmail.com>
2020-09-09 23:48:07 +0300

    .

---
+++ aelena.tf
@@ -0,0 +1,27 @@
+;; Sting of Aelena
+/def cast_sting_of_aelena = \
+    /send @target %{*};cast 'sting of aelena' %{*}
+
+/alias csa /cast_sting_of_aelena %{*}
+
+;; Bite of the black widow
+/def cast_bite_of_the_black_widow = \
+    /send @target %{*};cast 'bite of the black widow' %{*}
+
+/alias cb /cast_bite_of_the_black_widow %{*}
+
+/def cast_command_blade = \
+    /send @cast command blade
+
+/alias ccb /cast_command_blade
+
+/def use_wound = \
+    /send @target %{*};use 'wound' %{*}
+
+/alias uw /use_wound %{*}
+
+/def use_dissection = \
+    /send @use dissection at corpse try %{1} %{2}
+
+/alias ud /use_dissection %{*}
+

8cef1535ac661fc5478145ea826efa8582cd0592
Olli Aalto <oaalto@gmail.com>
2020-11-06 22:42:24 +0200

    .

--- disciple.tf
+++ disciple.tf
@@ -7,20 +7,20 @@
 ;; SKILLS
 
 ;; Chaotic Spawn
-/def use_chaotic_spawn =\
+/def use_chaotic_spawn = \
 	/send @use 'chaotic spawn' %{*}
 
 /alias ucs /use_chaotic_spawn %{*}
 
 ;; Clawed Strike
-/def use_clawed_strike =\
+/def use_clawed_strike = \
 	/send @target %{*};use 'clawed strike' %{*}
 
 /alias uc /use_clawed_strike %{*}
 
 ;; Kiss of Death
-/def use_kiss_of_death =\
-	/if (strlen({1}) > 0)\
+/def use_kiss_of_death = \
+	/if (strlen({1}) > 0) \
 		/send @target %{*};use kiss of death at %{*} %;\
 	/endif
 

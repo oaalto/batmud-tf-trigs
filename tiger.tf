be03429b0eaebe4f2bffc9f44692f4ccc9da0a20
Olli Aalto <oaalto@gmail.com>
2020-11-11 13:56:06 +0200

    .

--- tiger.tf
+++ tiger.tf
@@ -1,4 +1,5 @@
 /def -p10 -F -aCred -mregexp -t"(.+) manages to resist your claws!"
+/def -p10 -F -aCgreen -mregexp -t"As (.+) drops to his knees you leap in for the kill!"
 
 ;; Iron Palm
 /def use_iron_palm = \

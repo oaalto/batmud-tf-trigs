4e47372eb8f1ef85bdacfaa904a487a91791cb93
Olli Aalto <oaalto@gmail.com>
2021-01-04 16:02:51 +0200

    .

--- general.tf
+++ general.tf
@@ -1,39 +1,3 @@
-/load alias.tf
-/load batmud-tf-trigs/color_helpers.tf
-/load batmud-tf-trigs/status_line.tf
-/load batmud-tf-trigs/spellnames.tf
-/load batmud-tf-trigs/misc.tf
-;;/load batmud-tf-trigs/odefu.tf
-;;/load batmud-tf-trigs/fueryon.tf
-;;/load batmud-tf-trigs/cure_spells.tf
-;;/load batmud-tf-trigs/ceremony.tf
-;;/load batmud-tf-trigs/common_spells.tf
-
-;; Magical
-;;/load batmud-tf-trigs/magical.tf
-;;/load batmud-tf-trigs/riftwalker.tf
-;;/load batmud-tf-trigs/mage_magical.tf
-;;/load batmud-tf-trigs/mage_lightning.tf
-;;/load batmud-tf-trigs/mage_acid.tf
-;;/load batmud-tf-trigs/channellers.tf
-;;/load batmud-tf-trigs/psionicist.tf
-
-;; Evil Religious
-;;/load batmud-tf-trigs/disciple.tf
-;;/load batmud-tf-trigs/kharim.tf
-;;/load batmud-tf-trigs/nergal.tf
-;;/load batmud-tf-trigs/tzarakk.tf
-;;/load batmud-tf-trigs/aelena.tf
-;;/load batmud-tf-trigs/priest.tf
-;;/load batmud-tf-trigs/tiger.tf
-;;/load batmud-tf-trigs/reaver.tf
-;;/load batmud-tf-trigs/spider.tf
-
-;; Good Religious
-;;/load batmud-tf-trigs/monk.tf
-;;/load batmud-tf-trigs/liberator.tf
-;;/load batmud-tf-trigs/animist.tf
-
 /def -p10000 -F -mregexp -t'^[\*]+ Round .* [\*]+$' battle_round = \
 	/send @scan all %;\
 	/send @sc
@@ -143,4 +107,7 @@
 
 /def -p1000 -F -mregexp -t"You pay the ticketmaster (.+) gold, and he lets you board the ship." = \
 	/send @get 5 mithril from purse
+
+;; Aggro
+/def -p1000 -F -aCred -mregexp -t"got mad at hostile actions."
 
---
+++ loads.tf
@@ -0,0 +1,38 @@
+/load alias.tf
+/load batmud-tf-trigs/color_helpers.tf
+/load batmud-tf-trigs/status_line.tf
+/load batmud-tf-trigs/spellnames.tf
+/load batmud-tf-trigs/misc.tf
+/load batmud-tf-trigs/general.tf
+
+;;/load batmud-tf-trigs/odefu.tf
+;;/load batmud-tf-trigs/fueryon.tf
+;;/load batmud-tf-trigs/cure_spells.tf
+;;/load batmud-tf-trigs/ceremony.tf
+;;/load batmud-tf-trigs/common_spells.tf
+
+;; Magical
+;;/load batmud-tf-trigs/magical.tf
+;;/load batmud-tf-trigs/riftwalker.tf
+;;/load batmud-tf-trigs/mage_magical.tf
+;;/load batmud-tf-trigs/mage_lightning.tf
+;;/load batmud-tf-trigs/mage_acid.tf
+;;/load batmud-tf-trigs/channellers.tf
+;;/load batmud-tf-trigs/psionicist.tf
+
+;; Evil Religious
+;;/load batmud-tf-trigs/disciple.tf
+;;/load batmud-tf-trigs/kharim.tf
+;;/load batmud-tf-trigs/nergal.tf
+;;/load batmud-tf-trigs/tzarakk.tf
+;;/load batmud-tf-trigs/aelena.tf
+;;/load batmud-tf-trigs/priest.tf
+;;/load batmud-tf-trigs/tiger.tf
+;;/load batmud-tf-trigs/reaver.tf
+;;/load batmud-tf-trigs/spider.tf
+
+;; Good Religious
+;;/load batmud-tf-trigs/monk.tf
+;;/load batmud-tf-trigs/liberator.tf
+;;/load batmud-tf-trigs/animist.tf
+

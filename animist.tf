9a5df137fc7a0a89604ef7008e497ea1e6371fad
Olli Aalto <oaalto@gmail.com>
2021-02-09 20:14:44 +0200

    .

--- animist.tf
+++ animist.tf
@@ -8,34 +8,34 @@
 
 ;; Status line
 /def -p1000 -F -mregexp -ag -t'^Your soul companion: (.+) \((.+)%\)' = \
-        /let soul=$[decode_attr({P2}, colorer({P2}, 100))] %;\
+    /let soul=$[decode_attr({P2}, colorer({P2}, 100))] %;\
 	/let percent=$[set_white('%')] %;\
 	/set status_line_2=Soul: %{soul}%{percent}
 
-/def -p10000 -F -msimple -t'You start chanting.' start_spell =\
+/def -p10000 -F -msimple -t'You start chanting.' start_spell = \
 	/set cere_done=0
 
-/def -p10000 -F -msimple -t'You are done with the chant.' start_separating =\
+/def -p10000 -F -msimple -t'You are done with the chant.' start_separating = \
 	/set separating_soul=0 %;\
 	/set joining_soul=0 %;\
 	/set conjuring_mount=0 %;\
 	/set dismissing_mount=0
 
-/def -p10000 -F -msimple -t'You perform the ceremony.' cere_success =\
+/def -p10000 -F -msimple -t'You perform the ceremony.' cere_success = \
 	/set cere_done=1 %;\
-	/if ({separating_soul})\
+	/if ({separating_soul}) \
 		/cast_separate_soul %;\
-	/elseif ({joining_soul})\
+	/elseif ({joining_soul}) \
 		/cast_join_soul %;\
-	/elseif ({conjuring_mount})\
+	/elseif ({conjuring_mount}) \
 		/cast_conjure_mount %;\
-	/elseif ({dismissing_mount})\
+	/elseif ({dismissing_mount}) \
 		/cast_dismiss_mount %;\
 	/endif
 
 ;; Separate Soul
-/def cast_separate_soul =\
-	/if ({cere_done})\
+/def cast_separate_soul = \
+	/if ({cere_done}) \
 		/send @cast separate soul %;\
 	/else \
 		/set separating_soul=1 %;\
@@ -45,8 +45,8 @@
 /alias csoul /cast_separate_soul
 
 ;; Join Soul
-/def cast_join_soul =\
-	/if ({cere_done})\
+/def cast_join_soul = \
+	/if ({cere_done}) \
 		/send @cast join soul %;\
 	/else \
 		/set joining_soul=1 %;\
@@ -56,8 +56,8 @@
 /alias cjoin /cast_join_soul
 
 ;; Conjure Animal Soul
-/def cast_conjure_mount =\
-	/if ({cere_done})\
+/def cast_conjure_mount = \
+	/if ({cere_done}) \
 		/send @cast conjure animal soul %;\
 	/else \
 		/set conjuring_mount=1 %;\
@@ -67,8 +67,8 @@
 /alias csum /cast_conjure_mount
 
 ;; Dismiss Animal Soul
-/def cast_dismiss_mount =\
-	/if ({cere_done})\
+/def cast_dismiss_mount = \
+	/if ({cere_done}) \
 		/send @cast animal soul link at dismiss %;\
 	/else \
 		/set dismissing_mount=1 %;\
@@ -77,6 +77,6 @@
 
 /alias cdis /cast_dismiss_mount
 
-/def -p10000 -F -mregexp -t'(.+) spirit slowly appears, answering your call.' =\
+/def -p10000 -F -mregexp -t'(.+) spirit slowly appears, answering your call.' = \
 	/send @lead my spirit
 
--- disciple.tf
+++ disciple.tf
@@ -4,58 +4,52 @@
 
 /def -p10 -F -aBCred -msimple -t"You feel exhausted, being here in the light."
 
-/def -p1000 -F -mregexp -t"Polymorph spawn(.+) \| (\d+)min and (\d+)s" = \
-	/set spawn_mins=%{P2} %;\
-	/set spawn_secs=%{P3} %;\
-	/if ({mount_summoned}) \
-		/set spawn_timer=Spawn %{P2}mins %{P3}s %;\
-		/send @x %{tzarakk_mount} %;\
-	/else \
-		/set status_line_2=Spawn %{P2}mins %{P3}s %;\
-	/endif
+/set spawn_timer_on=0
 
-/def -p1000 -F -mregexp -t"Polymorph spawn(.+) \| (\d+)min" = \
-	/set spawn_mins=%{P2} %;\
-	/set spawn_secs=0 %;\
-	/if ({mount_summoned}) \
-		/set spawn_timer=Spawn %{P2}mins
-		/send @x %{tzarakk_mount} %;\
-	/else \
-		/set status_line_2=Spawn %{P2}mins %;\
-	/endif
+/def -p1000 -F -mregexp -t"Polymorph spawn(.+) \| (\d+)(mins|min) and (\d+)s" = \
+	/set_spawn_time %{P2} %{P4}
+
+/def set_spawn_time = \
+	/set spawn_mins=%{1} %;\
+	/set spawn_secs=%{2} %;\
+	/start_spawn_timer
+
+/def -p1000 -F -mregexp -t"Polymorph spawn(.+) \| (\d+)(min|mins)" = \
+	/set_spawn_time %{P2} 0
 
 /def -p1000 -F -mregexp -t"Polymorph spawn(.+) \| (\d+)s" = \
-	/set spawn_mins=0 %;\
-	/set spawn_secs=%{P2} %;\
+	/set_spawn_time 0 %{P2}
+
+/def decrease_spawn_timer = \
+	/echo timer goes: spawn_mins: %{spawn_mins} spawn_secs: %{spawn_secs} %;\
+	/let seconds=%{spawn_mins} * 60 + %{spawn_secs} - 1 %;\
+	/set spawn_mins=%{seconds} / 60 %;\
+	/set spawn_secs=%{seconds} - %{spawn_mins} * 60 %;\
 	/if ({mount_summoned}) \
-		/set spawn_timer=Spawn %{P2}s
+		/set spawn_timer=Spawn %{spawn_mins}mins %{spawn_secs}s %;\
 		/send @x %{tzarakk_mount} %;\
 	/else \
-		/set status_line_2=Spawn %{P2}s %;\
+		/set_spawn_status_line %;\
+	/endif %;\
+
+/def set_spawn_status_line = \
+	/if ({spawn_mins} > 0 & {spawn_secs} > 0) \
+		/set status_line_2=Spawn %{spawn_mins}mins %{spawn_secs}s %;\
+	/elseif ({spawn_mins} > 0) \
+		/set status_line_2=Spawn %{spawn_mins}mins %;\
+	/elseif ({spawn_secs} > 0) \
+		/set status_line_2=Spawn %{spawn_secs}s %;\
 	/endif
 
 /def start_spawn_timer = \
-	/set spawn_timer_pid=/repeat -0:01 i /decrease_spawn_timer
-
-/def decrease_spawn_timer = \
-	/let seconds=%{spawn_mins} * 60 + %{spawn_secs} - 1 %;\
-	/set spawn_mins=%{seconds} / 60 %;\
-	/set spawn_secs=%{seconds} - %{spawn_mins} * 60 &;\
-	/if ({mount_summoned}) \
-		/send @x %{tzarakk_mount} %;\
-	/else \
-		/if ({spawn_mins} > 0 && {spawn_secs} > 0) \
-			/set status_line_2=Spawn %{spawn_mins}mins %{spawn_secs}s %;\
-		/elseif ({spawn_mins} > 0) \
-			/set status_line_2=Spawn %{spawn_mins}mins %;\
-		/elseif ({spawn_secs} > 0) \
-			/set status_line_2=Spawn %{spawn_secs}s %;\
-		/endif %;\
+	/if ({spawn_timer_on} == 0) \
+		/set spawn_timer_on=1 %;\
+		/repeat -1 2 /decrease_spawn_timer %;\
 	/endif
 
 /def -p1010 -F -msimple -t"You pray for Azzarakk to recall his blessing." = \
 	/unset spawn_timer %;\
-	/kill %{spawn_timer_pid}
+	/set spawn_timer_on=0
 
 /def -p100 -F -msimple -t"You force yourself deeper into the chaos frenzy!" = \
 	/send @show effects

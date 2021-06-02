66ae703f9078be3f7678fdcf41d4438c774be293
Olli Aalto <oaalto@gmail.com>
2021-06-02 15:25:51 +0300

    .

--- fueryon.tf
+++ fueryon.tf
@@ -1,10 +1,10 @@
-/def -p1000 -F -aCgreen -mregexp -t"Fueryon hits (.+) (once|twice|thrice) (.+)."
+/def -p1000 -F -aCgreen -mregexp -t"Fueryon hits (.+) (once|twice|thrice|\d times) (.+)."
 /def -p1000 -F -P2Cblue -mregexp -t"Fueryon hits (.+) (once) (.+)."
 /def -p1000 -F -P2Cmagenta -mregexp -t"Fueryon hits (.+) (twice) (.+)."
 /def -p1000 -F -P2Cred -mregexp -t"Fueryon hits (.+) (thrice) (.+)."
 /def -p1000 -F -P2BCred -mregexp -t"Fueryon hits (.+) (\d+ times) (.+)."
 
-/def -p1000 -F -aCmagenta -mregexp -t"(.+) hits Fueryon (once|twice|thrice) (.+)."
+/def -p1000 -F -aCmagenta -mregexp -t"(.+) hits Fueryon (once|twice|thrice|\d times) (.+)."
 /def -p1000 -F -P2Cblue -mregexp -t"(.+) hits Fueryon (once) (.+)."
 /def -p1000 -F -P2BCmagenta -mregexp -t"(.+) hits Fueryon (twice) (.+)."
 /def -p1000 -F -P2Cred -mregexp -t"(.+) hits Fueryon (thrice) (.+)."
--- odefu.tf
+++ odefu.tf
@@ -1,10 +1,10 @@
-/def -p1000 -F -aCgreen -mregexp -t"Odefu hits (.+) (once|twice|thrice) (.+)."
+/def -p1000 -F -aCgreen -mregexp -t"Odefu hits (.+) (once|twice|thrice|\d times) (.+)."
 /def -p1000 -F -P2Cblue -mregexp -t"Odefu hits (.+) (once) (.+)."
 /def -p1000 -F -P2Cmagenta -mregexp -t"Odefu hits (.+) (twice) (.+)."
 /def -p1000 -F -P2Cred -mregexp -t"Odefu hits (.+) (thrice) (.+)."
 /def -p1000 -F -P2BCred -mregexp -t"Odefu hits (.+) (\d+ times) (.+)."
 
-/def -p1000 -F -aCmagenta -mregexp -t"(.+) hits Odefu (once|twice|thrice) (.+)."
+/def -p1000 -F -aCmagenta -mregexp -t"(.+) hits Odefu (once|twice|thrice|\d times) (.+)."
 /def -p1000 -F -P2Cblue -mregexp -t"(.+) hits Odefu (once) (.+)."
 /def -p1000 -F -P2BCmagenta -mregexp -t"(.+) hits Odefu (twice) (.+)."
 /def -p1000 -F -P2Cred -mregexp -t"(.+) hits Odefu (thrice) (.+)."

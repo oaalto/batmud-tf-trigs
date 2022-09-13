ff191548c0fed1a2c7160bb9b1ff57438eb78bce
Olli Aalto <oaalto@gmail.com>
2022-09-13 22:53:13 +0300

    .

---
+++ ranger.tf
@@ -0,0 +1,19 @@
+;; Start Combat
+/def start_combat = \
+  /use_bladed_fury %{*} %;\
+  /if (strlen({1]) > 0) \
+    /send @kill %{*} %;\
+  /endif %;\
+
+/alias cs /start_combat %{*}
+
+;; Bladed Fury
+/def use_bladed_fury = \
+  /if (strlen({1}) > 0) \
+		/send @target %{*} %;\
+  /endif %;\
+  /send use 'bladed fury' %{*}
+
+/alias ubf /use_bladed_fury %{*}
+
+

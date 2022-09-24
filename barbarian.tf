e0dfb861a847faeae62875c8869960b08895e61e
Olli Aalto <oaalto@gmail.com>
2022-09-24 15:59:38 +0300

    .

--- barbarian.tf
+++ barbarian.tf
@@ -1,5 +1,5 @@
 ;; Barbburn rip action
-/def barb_rip_action = /send @rip_action set get all frmo corpse;light torch;barbburn;extinghuis torch;drop zinc;drop mowgles
+/def barb_rip_action = /send @rip_action set get all from corpse;light torch;barbburn;extinguish torch;drop zinc;drop mowgles
 
 /alias barb_rip /barb_rip_action
 

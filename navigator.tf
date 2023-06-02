3268ba87ff3c1a4eee4f8e2bb3b8f042c27d9d36
Olli Aalto <oaalto@gmail.com>
2023-06-02 07:57:36 +0300

    .

---
+++ navigator.tf
@@ -0,0 +1,15 @@
+/def cast_teleport_with_error = \
+	/send @cast teleport with error
+
+/alias ctwe /cast_teleport_with_error
+
+/def cast_teleport_without_error = \
+  /send @cast teleport without error
+
+/alias ctw /cast_teleport_without_error
+
+/def cast_relocate = \
+	/send @cast 'relocate' %{*}
+
+/alias cr /cast_relocate %{*}
+

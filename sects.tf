d417b6d6c6fb0ca4aa54d3f9feee4286f36e1ca2
Olli Aalto <oaalto@gmail.com>
2026-04-30 13:15:29 +0300

    .

--- sects.tf
+++ sects.tf
@@ -1,3 +1,4 @@
 /def -p10 -aCgreen -mregexp -t"You have finished cultivating (.+)"
 /def -p10 -aCyellow -mregexp -t"Your mantra of (.+) is no longer active."
+/def -p10 -aCred -mregexp -t"You decide not to recite the (.+) as you are not actively cultivating anything."
 
